b73d411eaf0797d239331b1524e0a3f873defe18
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-01-27 03:22:00 +0900

    feat: DOM을 숨기고 보이게 할 수 있는 함수 구현(show, hide)

--- src/utils/DOM.ts
+++ src/utils/DOM.ts
@@ -3,4 +3,8 @@ export const $$ = (selector: string) => document.querySelectorAll(selector);
 
 export const clearInputValue = ($target: HTMLInputElement) => ($target.value = '');
 
+export const show = ($target: HTMLElement) => $target.classList.remove('d-none');
+export const hide = ($target: HTMLElement) => $target.classList.add('d-none');
+
 export const disable = ($target: HTMLInputElement | HTMLButtonElement) => ($target.disabled = true);
+export const enable = ($target: HTMLInputElement | HTMLButtonElement) => ($target.disabled = false);
