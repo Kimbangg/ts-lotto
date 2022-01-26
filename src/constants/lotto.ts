28ba920c86735df1e970291e5e816bdeecae6d78
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-01-27 01:44:59 +0900

    refactor: 상수 객체 이름 변경 & Object.freeze 적용

--- src/constants/lotto.ts
+++ src/constants/lotto.ts
@@ -1,9 +1,12 @@
-export const PURCAHSE_RULE = {
+export const LOTTO = Object.freeze({
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+  LENGTH: 6,
   SALE_UNIT: 1000,
   MINIMUN_PRICE: 1000,
-};
+});
 
 export const PUCHASE_ALERT_MESSAGE = {
-  IS_INVALID_AMOUNT: `로또는 ${PURCAHSE_RULE.SALE_UNIT}원 단위로만 구매할 수 있습니다`,
-  IS_LOWER_THAN_MINIMUM_AMOUNT: `값은 ${PURCAHSE_RULE.MINIMUN_PRICE} 이상 이어야 합니다.`,
+  IS_INVALID_AMOUNT: `로또는 ${LOTTO.SALE_UNIT}원 단위로만 구매할 수 있습니다`,
+  IS_LOWER_THAN_MINIMUM_AMOUNT: `값은 ${LOTTO.MINIMUN_PRICE} 이상 이어야 합니다.`,
 };
