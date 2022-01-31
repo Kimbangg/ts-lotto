b619d48ca1d33e88737eb3066b221d61308e7e0f
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-01-31 23:43:15 +0900

    refactor: 로또 당첨번호 입력 시, 에러를 메세지를 통해 렌더링

--- src/components/WinningNumbersInput.ts
+++ src/components/WinningNumbersInput.ts
@@ -1,21 +1,69 @@
+import { LOTTO, WINNING_INPUT_ALERT_MESSAGE } from '@/constants/lotto';
 import { BaseComponent } from '@/core/Component';
 import { $, $$, hide, show } from '@/utils/DOM';
 
 interface Props {
   isPurchased: boolean;
   lottoTickets: Array<Number[]>;
-  setLottoResult: (winningLottoNumber: Number[]) => void;
+  setLottoResult: (winningLottoNumber: Number[], bonusNumber: number) => void;
 }
+
+const isEmptyValue = (value: number | string) => value === '';
+const isValidRange = (value: number | string) => {
+  if (value && typeof value === 'number') {
+    return LOTTO.MIN_NUMBER <= value && value <= LOTTO.MAX_NUMBER;
+  }
+};
+
+const isValidInput = (winningNumberInput: Array<number | string>, bonusNumber: number | string) => {
+  const totalWinningNumbers = [...winningNumberInput, bonusNumber];
+  const { EMPTY_INPUT_NUMBER, OUT_OF_RANGE, DUPLICATED_NUMBER, VALID_WINNING_INPUT_NUMBER } =
+    WINNING_INPUT_ALERT_MESSAGE;
+
+  if (totalWinningNumbers.some(isEmptyValue)) {
+    return {
+      isError: true,
+      errorMessage: EMPTY_INPUT_NUMBER,
+    };
+  }
+
+  if (!totalWinningNumbers.every(isValidRange)) {
+    return {
+      isError: true,
+      errorMessage: OUT_OF_RANGE,
+    };
+  }
+
+  if (new Set(totalWinningNumbers).size !== totalWinningNumbers.length) {
+    return {
+      isError: true,
+      errorMessage: DUPLICATED_NUMBER,
+    };
+  }
+
+  return {
+    isError: false,
+    errorMessage: VALID_WINNING_INPUT_NUMBER,
+  };
+};
 
 export class WinningNumbersInput extends BaseComponent<HTMLElement, Props> {
   $winningInputForm: HTMLFormElement;
+  $bonusInput: HTMLInputElement;
   $winningInputs: NodeListOf<HTMLInputElement>;
   $winningSubmitButton: HTMLInputElement;
+  $winningErrorMessage: HTMLParagraphElement;
 
   selectDom() {
     this.$winningInputForm = $('#winning-input-form')! as HTMLFormElement;
     this.$winningInputs = $$('.winning-number')! as NodeListOf<HTMLInputElement>;
+    this.$bonusInput = $('.bonus-number')! as HTMLInputElement;
     this.$winningSubmitButton = $('#winning-input-form__submit')! as HTMLInputElement;
+    this.$winningErrorMessage = $('.winning-input-form__error-message')! as HTMLParagraphElement;
+  }
+
+  requestValidInput(errorMessage: string) {
+    alert(errorMessage);
   }
 
   onSubmitWinningNumbers() {
@@ -25,20 +73,56 @@ export class WinningNumbersInput extends BaseComponent<HTMLElement, Props> {
       Number(winningNumber.value)
     );
 
-    setLottoResult(winningLottoNumber);
+    const bonusNumber = Number(this.$bonusInput.value);
+
+    setLottoResult(winningLottoNumber, bonusNumber);
   }
 
-  onKeyUpNumberInput() {}
+  onMoveFocusToNextInput({ target }: { target: any }) {
+    if (target.value.length > 1) {
+      target.value = target.value.slice(0, 2);
+
+      (target.nextElementSibling ? target.nextElementSibling : this.$bonusInput).focus();
+    }
+  }
+
+  onKeyUpNumberInput(event: KeyboardEvent) {
+    const { $winningInputs, $bonusInput, $winningErrorMessage, $winningSubmitButton } = this;
+    this.onMoveFocusToNextInput(event);
+
+    const winningNumbers = Array.from($winningInputs).map(input =>
+      input.value === '' ? '' : Number(input.value)
+    );
+
+    const bonusNumber = $bonusInput.value === '' ? '' : Number(this.$bonusInput.value);
+
+    const { isError, errorMessage } = isValidInput(winningNumbers, bonusNumber);
+
+    if (isError) {
+      $winningErrorMessage.style.color = 'red';
+      $winningSubmitButton.disabled = true;
+    } else {
+      $winningErrorMessage.style.color = 'green';
+      $winningSubmitButton.disabled = false;
+    }
+
+    $winningErrorMessage.textContent = errorMessage;
+  }
 
   setEvent() {
-    const { $winningInputs, $winningInputForm, $winningSubmitButton } = this;
+    const { $winningInputs, $bonusInput, $winningSubmitButton } = this;
 
-    // TODO: Validation을 위한 keyup Event
-    $winningInputForm.onkeyup = () => {
-      this.onKeyUpNumberInput();
+    $winningInputs.forEach($input => {
+      $input.onkeyup = event => {
+        this.onKeyUpNumberInput(event);
+      };
+    });
+
+    $bonusInput.onkeyup = event => {
+      this.onKeyUpNumberInput(event);
     };
 
-    $winningInputForm.onsubmit = event => {
+    $winningSubmitButton.onclick = event => {
       event.preventDefault();
       this.onSubmitWinningNumbers();
     };
