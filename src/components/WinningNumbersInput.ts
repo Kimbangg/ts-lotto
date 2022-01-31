cffecdfbbebbbd055bdaebb08c09da533c4b5f36
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-01-31 17:42:13 +0900

    feat: setLottoResult Method를 WinningNumberInputs에 전달 및 적용

--- src/components/WinningNumbersInput.ts
+++ src/components/WinningNumbersInput.ts
@@ -1,28 +1,51 @@
 import { BaseComponent } from '@/core/Component';
-import { $, hide, show } from '@/utils/DOM';
+import { $, $$, hide, show } from '@/utils/DOM';
 
 interface Props {
   isPurchased: boolean;
   lottoTickets: Array<Number[]>;
+  setLottoResult: (winningLottoNumber: Number[]) => void;
 }
 
 export class WinningNumbersInput extends BaseComponent<HTMLElement, Props> {
   $winningInputForm: HTMLFormElement;
+  $winningInputs: NodeListOf<HTMLInputElement>;
+  $winningSubmitButton: HTMLInputElement;
+
   selectDom() {
     this.$winningInputForm = $('#winning-input-form')! as HTMLFormElement;
+    this.$winningInputs = $$('.winning-number')! as NodeListOf<HTMLInputElement>;
+    this.$winningSubmitButton = $('#winning-input-form__submit')! as HTMLInputElement;
   }
 
+  onSubmitWinningNumbers() {
+    const { setLottoResult } = this.props;
+
+    const winningLottoNumber = Array.from(this.$winningInputs).map(winningNumber =>
+      Number(winningNumber.value)
+    );
+
+    setLottoResult(winningLottoNumber);
+  }
+
+  onKeyUpNumberInput() {}
+
   setEvent() {
-    // toggle을 눌렀을 때 반응하는 이벤트를 등록
+    const { $winningInputs, $winningInputForm, $winningSubmitButton } = this;
+
+    // TODO: Validation을 위한 keyup Event
+    $winningInputForm.onkeyup = () => {
+      this.onKeyUpNumberInput();
+    };
+
+    $winningInputForm.onsubmit = event => {
+      event.preventDefault();
+      this.onSubmitWinningNumbers();
+    };
   }
 
   render() {
     const { isPurchased, lottoTickets } = this.props;
-
-    if (!isPurchased) {
-      hide(this.$winningInputForm);
-    } else {
-      show(this.$winningInputForm);
-    }
+    isPurchased === false ? hide(this.$winningInputForm) : show(this.$winningInputForm);
   }
 }
