b3feb758972a0e05862cf5562c300a349d8505d5
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-02-01 19:48:10 +0900

    feat: 순위를 결과 모달 창에 반영하는 메소드 추가 및 적용

--- src/components/WinningResultDisplay.ts
+++ src/components/WinningResultDisplay.ts
@@ -1,5 +1,6 @@
 import { $, open, close } from './../utils/DOM';
 import { BaseComponent } from '@/core/Component';
+import { NAME_FOR_LOTTO_RANK } from '@/constants/lotto';
 
 interface Props {
   winningResult: Record<number, number>;
@@ -7,17 +8,28 @@ interface Props {
 }
 
 export class WinningResultDisplay extends BaseComponent<HTMLElement, Props> {
+  $modalClose: HTMLDivElement;
   $resultDisplayModal: HTMLDivElement;
 
   selectDom() {
+    this.$modalClose = $('.modal-close')! as HTMLDivElement;
     this.$resultDisplayModal = $('#result-display-modal')! as HTMLDivElement;
   }
 
-  updateResultModalView() {}
+  updateResultModalView() {
+    const { winningResult } = this.props;
+
+    Object.entries(winningResult).forEach(result => {
+      const eleName = NAME_FOR_LOTTO_RANK[result[0]];
+      const $elem = $(eleName)! as HTMLTableElement;
+
+      $elem.textContent = `${result[1]}개`;
+    });
+  }
 
   render() {
-    const { hasWinningResult } = this.props;
     const { $resultDisplayModal } = this;
+    const { hasWinningResult } = this.props;
 
     this.updateResultModalView();
     hasWinningResult ? open($resultDisplayModal) : close($resultDisplayModal);
